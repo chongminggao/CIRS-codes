78bb09e64ae633fd34088bea971dbee8cb0d67b7
ShiqiWang <49370449+Strawberry47@users.noreply.github.com>
2022-03-09 23:02:47 +0800

    Update layers.py

--- core/layers.py
+++ core/layers.py
@@ -15,74 +15,7 @@ from deepctr_torch.layers import PredictionLayer, activation_layer
 from deepctr_torch.callbacks import History
 from torch import Tensor
 
-class DNN(nn.Module):
-    """The Multi Layer Percetron
 
-      Input shape
-        - nD tensor with shape: ``(batch_size, ..., input_dim)``. The most common situation would be a 2D input with shape ``(batch_size, input_dim)``.
-
-      Output shape
-        - nD tensor with shape: ``(batch_size, ..., hidden_size[-1])``. For instance, for a 2D input with shape ``(batch_size, input_dim)``, the output would have shape ``(batch_size, hidden_size[-1])``.
-
-      Arguments
-        - **inputs_dim**: input feature dimension.
-
-        - **hidden_units**:list of positive integer, the layer number and units in each layer.
-
-        - **activation**: Activation function to use.
-
-        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix.
-
-        - **dropout_rate**: float in [0,1). Fraction of the units to dropout.
-
-        - **use_bn**: bool. Whether use BatchNormalization before activation or not.
-
-        - **seed**: A Python integer to use as random seed.
-    """
-
-    def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
-                 init_std=0.0001, dice_dim=3, seed=1024, device='cpu'):
-        super(DNN, self).__init__()
-        self.dropout_rate = dropout_rate
-        self.dropout = nn.Dropout(dropout_rate)
-        self.seed = seed
-        self.l2_reg = l2_reg
-        self.use_bn = use_bn
-        if len(hidden_units) == 0:
-            raise ValueError("hidden_units is empty!!")
-        hidden_units = [inputs_dim] + list(hidden_units)
-
-        self.linears = nn.ModuleList(
-            [nn.Linear(hidden_units[i], hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
-
-        if self.use_bn:
-            self.bn = nn.ModuleList(
-                [nn.BatchNorm1d(hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
-
-        self.activation_layers = nn.ModuleList(
-            [activation_layer(activation, hidden_units[i + 1], dice_dim) for i in range(len(hidden_units) - 1)])
-
-        for name, tensor in self.linears.named_parameters():
-            if 'weight' in name:
-                nn.init.normal_(tensor, mean=0, std=init_std)
-
-        self.to(device)
-
-    def forward(self, inputs):
-        deep_input = inputs
-
-        for i in range(len(self.linears)):
-
-            fc = self.linears[i](deep_input)
-
-            if self.use_bn:
-                fc = self.bn[i](fc)
-
-            fc = self.activation_layers[i](fc)
-
-            fc = self.dropout(fc)
-            deep_input = fc
-        return deep_input
 
 class Linear(nn.Module):
     def __init__(self, feature_columns, feature_index, init_std=0.0001, device='cpu', padding_idx=None):
